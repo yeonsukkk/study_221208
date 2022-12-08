2ecff6aa3ed3e86d46c448de15f7f19d1c0d8854
yeonsukkk <dustn910720@gmail.com>
2022-12-08 16:58:29 +0900

    설명글추가

--- js/index04.js
+++ js/index04.js
@@ -4,7 +4,7 @@ const user = {
     completed: true
 }
 
-fetch('https://jsonplaceholder.typicode.com/todos', {method: 'POST', body: JSON.stringify(user)}).
+fetch('https://jsonplaceholder.typicode.com/todos', {method: 'POST', body: JSON.stringify(user)}). // 추가된 데이터를 제이슨파일로 보내야함
 then(response => response.text()).
 then(result => {
     console.log(result)
